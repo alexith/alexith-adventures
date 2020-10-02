9b46d79af4b529112f4ebf94e52f892cb94f8ac0
home <akshay.cy2@gmail.com>
2020-10-02 10:59:27 -0400

    first commit

---
+++ BugController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BugController : MonoBehaviour
+{
+    public float moveSpeed;
+    private Rigidbody2D myRigidbody;
+
+    private bool moving;
+
+    public float timeBetweenMove;
+    private float timeBetweenMoveCounter;
+
+    public float timeToMove;
+    private float timeToMoveCounter;
+
+    public Vector3 moveDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       myRigidbody = GetComponent<Rigidbody2D>();
+       timeBetweenMoveCounter = timeBetweenMove;
+       timeToMoveCounter = timeToMove;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moving)
+        {
+            timeToMoveCounter -= Time.deltaTime;
+            myRigidbody.velocity = moveDirection;
+
+            if(timeToMoveCounter < 0f)
+            {
+                moving = false;
+                timeBetweenMoveCounter = timeBetweenMove;
+            }
+        } else {
+            timeBetweenMoveCounter -= Time.deltaTime;
+            myRigidbody.velocity = Vector2.zero;
+
+            if(timeBetweenMoveCounter < 0f)
+            {
+                moving = true;
+                timeToMoveCounter = timeToMove;
+
+                moveDirection = new Vector3(Random.Range (-1f, 1f) * moveSpeed, Random.Range(-1f, 1f) * moveSpeed, 0f);
+            }
+        }
+    }
+}
+
+
